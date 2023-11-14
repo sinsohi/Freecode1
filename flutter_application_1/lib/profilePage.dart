6e770667c1f934f254e5784f71bba113d192de12
sinsohi_04 <sohi8451@naver.com>
2023-11-14 18:52:49 +0900

    'LogOut_Button'

--- flutter_application_1/lib/profilePage.dart
+++ flutter_application_1/lib/profilePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class profilePage extends StatelessWidget {
   const profilePage({super.key});
@@ -6,8 +7,16 @@ class profilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('여기는 profile페이지'),
-    );
+        appBar: AppBar(),
+        body: IconButton(
+          onPressed: () {
+            //로그아웃 버튼
+            FirebaseAuth.instance.signOut();
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.exit_to_app_sharp,
+              color: const Color.fromARGB(255, 8, 8, 8)),
+        ));
   }
 }
 
