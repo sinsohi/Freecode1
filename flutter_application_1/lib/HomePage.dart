c2cdd56d069da2b9291c9461308344dd6c310989
minseonju <10sc1108@naver.com>
2023-11-15 18:42:27 +0900

    페이지이동 q

--- flutter_application_1/lib/HomePage.dart
+++ flutter_application_1/lib/HomePage.dart
@@ -14,78 +14,100 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(55, 115, 108, 1),
-        title: Container(
-          child: Row(
-            children: const [
-              Icon(
-                Icons.account_circle,
-                color: Color.fromRGBO(248, 246, 232, 1),
-                size: 50,
-              ),
-              SizedBox(width: 10), //아이콘이랑 글자사이의 공백을 조절하기 위한 박스얌
-              Text(
-                'welcome!',
-                style: TextStyle(
-                  color: Color.fromRGBO(248, 246, 232, 1),
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.5,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(55, 115, 108, 1),
+          title: Container(
+            child: Row(
+              children: [
+                Icon(Icons.account_circle, color: Color.fromRGBO(248, 246, 232, 1), size: 50,),
+                SizedBox(width: 10),
+                Text(
+                  'welcome!',
+                  style: TextStyle(
+                    color: Color.fromRGBO(248, 246, 232, 1),
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5,
+                  ),
                 ),
-              ),
-              // Image.asset(
-              //   'img-removebg-preview.png',
-              //   fit: BoxFit.cover,
-              //   height: 50,
-              //   width: 50,
-              // ),
-            ],
+               // Image.asset(
+                  //'img-removebg-preview.png',
+                  //fit: BoxFit.cover,
+                  //height: 50,
+                  //width: 50,
+                //),
+              ],
+            ),
           ),
         ),
-      ),
-      body: Container(color: Color.fromRGBO(248, 246, 232, 1),),
-
-
-
-      bottomNavigationBar: BottomAppBar(
-        height: 50,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-                onPressed: () {
-                  // 캘린더 페이지로 이동
+        body: Container(color: Color.fromRGBO(248, 246, 232, 1),),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            boxShadow: const <BoxShadow>[
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 15,
+              ),
+            ],
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: Color.fromRGBO(55, 115, 108, 1),
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Color.fromRGBO(248, 246, 232, 1),
+            unselectedItemColor: Color.fromRGBO(248, 246, 232, 1),
+            selectedLabelStyle: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+            unselectedLabelStyle: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+            showSelectedLabels: true,
+            showUnselectedLabels: false,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: '홈',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_month),
+                label: '캘린더',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.bar_chart_sharp),
+                label: '통계자료',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.people),
+                label: '마이페이지',
+              ),
+            ],
+            onTap: (int index) {
+              switch (index) {
+                case 0:
+                // 홈 페이지로 이동 (아직 구현되지 않음)
+                  break;
+                case 1:
+                // 캘린더 페이지로 이동
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => calendarPage(),
-                      ));
-                },
-                icon: Icon(Icons.calendar_month)),
-            Icon(Icons.home),
-            IconButton(
-                onPressed: () {
-                  // 그래프 페이지로 이동
+                    context,
+                    MaterialPageRoute(builder: (context) => calendarPage()),
+                  );
+                  break;
+                case 2:
+                // 통계자료 페이지로 이동
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => graph(),
-                      ));
-                },
-                icon: Icon(Icons.bar_chart_sharp)),
-            IconButton(
-                onPressed: () {
-                  // 프로필 페이지로 이동
+                    context,
+                    MaterialPageRoute(builder: (context) => graph()),
+                  );
+                  break;
+                case 3:
+                // 마이페이지로 이동
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => profilePage(),
-                      ));
-                },
-                icon: Icon(Icons.person)),
-          ],
+                    context,
+                    MaterialPageRoute(builder: (context) => profilePage()),
+                  );
+                  break;
+              }
+            },
+          ),
         ),
       ),
     );
