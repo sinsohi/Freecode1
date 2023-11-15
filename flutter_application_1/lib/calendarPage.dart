fe5670321a8842474aa2f652cc205e7fb3b2baa8
kmingseo <ms7happy@naver.com>
2023-11-15 15:58:16 +0900

    달력 구조 수정

--- flutter_application_1/lib/calendarPage.dart
+++ flutter_application_1/lib/calendarPage.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class calendarPage extends StatelessWidget {
-  const calendarPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: TableCalendar(
-        firstDay: DateTime.utc(2021, 10, 16),
-        lastDay: DateTime.utc(2030, 3, 14),
-        focusedDay: DateTime.now(),
-      ),
-    );
-  }
-}
-
 void main() {
   runApp(MaterialApp(
     home: calendarPage(),
   ));
+}
+
+class calendarPage extends StatefulWidget {
+  const calendarPage({super.key});
+
+  @override
+  State<calendarPage> createState() => _calendarPageState();
+}
+
+class _calendarPageState extends State<calendarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: content(),
+    );
+  }
+
+  Widget content() {
+    return Column(
+      children: [
+        Container(
+          child: TableCalendar(
+            firstDay: DateTime.utc(2021, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            focusedDay: DateTime.now(),
+          ),
+        )
+      ],
+    );
+  }
 }
