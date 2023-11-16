59334b73886af796aad9e42a8a6541bd83bbe77d
sinsohi_04 <sohi8451@naver.com>
2023-11-16 14:24:05 +0900

    backgroundColor

--- flutter_application_1/lib/profilePage.dart
+++ flutter_application_1/lib/profilePage.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_application_1/HomePage.dart';
+import 'calendarPage.dart';
+import 'graph.dart';
+import 'HomePage.dart';
 
 class profilePage extends StatelessWidget {
   const profilePage({super.key});
@@ -7,16 +11,83 @@ class profilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: IconButton(
-          onPressed: () {
-            //로그아웃 버튼
-            FirebaseAuth.instance.signOut();
-            Navigator.pop(context);
-          },
-          // 이건 변경할 사항
-          icon: Icon(Icons.exit_to_app_sharp,
-              color: const Color.fromARGB(255, 8, 8, 8)),
+        body: Container(),
+        backgroundColor: Color.fromRGBO(55, 115, 108, 100),
+        // IconButton(
+        //   onPressed: () {
+        //     //로그아웃 버튼
+        //     FirebaseAuth.instance.signOut();
+        //     Navigator.pop(context);
+        //   },
+
+        //   icon: Icon(Icons.exit_to_app_sharp,
+        //       color: const Color.fromARGB(255, 8, 8, 8)),
+        // ),
+
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            boxShadow: const <BoxShadow>[
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 15,
+              ),
+            ],
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: Color.fromRGBO(55, 115, 108, 1),
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Color.fromRGBO(248, 246, 232, 1),
+            unselectedItemColor: Color.fromRGBO(248, 246, 232, 1),
+            selectedLabelStyle:
+                TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+            unselectedLabelStyle:
+                TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+            showSelectedLabels: true,
+            showUnselectedLabels: false,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: '홈',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_month),
+                label: '캘린더',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.bar_chart_sharp),
+                label: '통계자료',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.people),
+                label: '마이페이지',
+              ),
+            ],
+            onTap: (int index) {
+              switch (index) {
+                case 0:
+                  // 홈 페이지로 이동
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                  );
+                  break;
+                case 1:
+                  // 캘린더 페이지로 이동
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => calendarPage()),
+                  );
+                  break;
+                case 2:
+                  // 통계자료 페이지로 이동
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => graph()),
+                  );
+                  break;
+              }
+            },
+          ),
         ));
   }
 }
