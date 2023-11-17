0a0e8053e7f16ac9c931999de257c32178e82de0
starsaverKDH <pindoll76@naver.com>
2023-11-17 10:32:10 +0900

    막대그래프 추가.(사이즈 조정 예정)

--- flutter_application_1/lib/graph.dart
+++ flutter_application_1/lib/graph.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:fl_chart/fl_chart.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:syncfusion_flutter_charts/sparkcharts.dart';
-
+//import 'package:fl_chart/fl_chart.dart';
+//import 'package:syncfusion_flutter_charts/charts.dart';
+//import 'package:syncfusion_flutter_charts/sparkcharts.dart';
+import 'package:unique_simple_bar_chart/data_models.dart';
+import 'package:unique_simple_bar_chart/horizontal_bar.dart';
+import 'package:unique_simple_bar_chart/horizontal_line.dart';
+import 'package:unique_simple_bar_chart/simple_bar_chart.dart';
 import 'package:pie_chart/pie_chart.dart';
 import 'dart:math' as math;
-
-
-
 
 class PieModel {
   final int count;
@@ -37,25 +37,88 @@ class _graphState extends State<graph> {
       PieModel(count: 10, color: Color.fromARGB(255, 255, 233, 65).withOpacity(1)),
       PieModel(count: 2, color: Color.fromARGB(255, 124, 205, 127).withOpacity(1)),
       PieModel(count: 30, color: Colors.cyan.withOpacity(1)),
-      PieModel(count: 20, color: const Color.fromARGB(255, 156, 89, 168).withOpacity(1)),
+      PieModel(count: 20, color: Color.fromARGB(255, 255, 143, 238).withOpacity(1)),
     ];
     
     return Scaffold(
       appBar: AppBar(title: Text('graph')),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center, //중앙정렬
         children: [
-          SizedBox(
+          Container( //원래 Sizedbox였는데 좀 바꿈
             width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.width,
+            height: 10,
             child: CustomPaint(
               size: Size(MediaQuery.of(context).size.width,
                   MediaQuery.of(context).size.width),
               painter: _PieChart(model),
             ),
           ),
-        ],
+          //여기에 막대그래프 추가
+          Container(
+            //막대그래프 코드
+            height: 100,
+        width: MediaQuery.of(context).size.width,
+        child: SimpleBarChart(
+          makeItDouble: true,
+          listOfHorizontalBarData: [
+            HorizontalDetailsModel(
+              name: 'Mon',
+              color: const Color(0xFFEB7735),
+              size: 73,
+              sizeTwo: 40,
+              colorTwo: Colors.blue,
+            ),
+            HorizontalDetailsModel(
+              name: 'Tues',
+              color: const Color(0xFFEB7735),
+              size: 92,
+              sizeTwo: 85,
+              colorTwo: Colors.blue,
+            ),
+            HorizontalDetailsModel(
+              name: 'Wed',
+              color: const Color(0xFFFBBC05),
+              size: 120,
+              sizeTwo: 100,
+              colorTwo: Colors.blue,
+            ),
+            HorizontalDetailsModel(
+              name: 'Thurs',
+              color: const Color(0xFFFBBC05),
+              size: 86,
+              sizeTwo: 220,
+              colorTwo: Colors.blue,
+            ),
+            HorizontalDetailsModel(
+              name: 'Fri',
+              color: const Color(0xFFFBBC05),
+              size: 64,
+              sizeTwo: 170,
+              colorTwo: Colors.blue,
+            ),
+            HorizontalDetailsModel(
+              name: 'Sat',
+              color: const Color(0xFFFBBC05),
+              size: 155,
+              sizeTwo: 120,
+              colorTwo: Colors.blue,
+            ),
+            HorizontalDetailsModel(
+              name: 'Sun',
+              color: const Color(0xFFFBBC05),
+              size: 200,
+              sizeTwo: 96,
+              colorTwo: Colors.blue,
+            ),
+          ],
+          verticalInterval: 100,
+          horizontalBarPadding: 20,
+          )
+      )],
       ),
     );
+    
   }
 }
 
