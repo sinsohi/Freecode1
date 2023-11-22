0b2a55c4118d98a091fcf372f9a432890eff78f9
starsaverKDH <pindoll76@naver.com>
2023-11-22 16:20:12 +0900

    카테고리 5개 고정

--- flutter_application_1/lib/graph.dart
+++ flutter_application_1/lib/graph.dart
@@ -13,8 +13,93 @@ import 'calendarPage.dart'; //바텀네비게이션바
 import 'graph.dart';
 import 'profilePage.dart';
 import 'package:table_calendar/table_calendar.dart'; // 15~16 현재 월 표시
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart'; 
+import 'package:firebase_auth/firebase_auth.dart';//데이터베이스 가져오기
+import 'package:firebase_database/firebase_database.dart';
 
+
+
+Future<Map<String, dynamic>> getTotalAmountByCategory() async {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  User? user = auth.currentUser;
+
+  if (user == null) {
+    // 사용자가 로그인되어 있지 않은 경우
+    return {'error': 'User not logged in'};
+  }
+
+  String uid = user.uid;
+
+  DatabaseReference expensesRef = FirebaseDatabase.instance.ref().child('expenses');
+
+  DatabaseEvent event = await expensesRef.orderByChild('type').equalTo('expense').once();
+
+  Map<String, double> categoryTotalAmounts = {};
+
+  if (event.snapshot != null) {
+    (event.snapshot!.value as Map).forEach((key, value) {
+      if (value['uid'] == uid) {
+        String category = value['category'];
+        double amount = (value['amount'] ?? 0.0).toDouble();
+
+        categoryTotalAmounts.update(category, (value) => value + amount, ifAbsent: () => amount);
+      }
+    });
+  }
+
+  return {'categoryTotalAmounts': categoryTotalAmounts};
+}
+
+Future<double> getTotalAmountForCurrentMonth() async {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  User? user = auth.currentUser;
+
+  if (user == null) {
+    // 사용자가 로그인되어 있지 않은 경우
+    return 0.0;
+  }
+
+  String uid = user.uid;
+
+  DatabaseReference expensesRef = FirebaseDatabase.instance.ref().child('expenses');
+
+  DateTime now = DateTime.now();
+  String currentMonth = DateFormat('yyyy-MM').format(now);
+
+  DatabaseEvent event = await expensesRef.orderByChild('date').startAt('$currentMonth-01').endAt('$currentMonth-31').once();
+
+  double totalAmount = 0.0;
+
+  if (event.snapshot != null) {
+    (event.snapshot!.value as Map).forEach((key, value) {
+      if (value['uid'] == uid) {
+        double amount = (value['amount'] ?? 0.0).toDouble();
+        totalAmount += amount;
+      }
+    });
+  }
+
+  return totalAmount;
+}
+//이까지 uid,'category' 같은 것들끼리 분류해서 amount를 모두 더한 값 구하는 함수(category, amount 반환),  사용자 이번 달 모든 amount 더한 값 반환 함수
+
+
+Future<void> fetchDataForCurrentUser() async {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  User? user = auth.currentUser;
+
+  if (user != null) {
+    String uid = user.uid;
+
+    // 여기서 uid를 사용하여 데이터베이스에서 사용자별 데이터를 가져올 수 있음
+    print('Current user UID: $uid');
+
+    // 이후에 데이터베이스 관련 작업 수행
+  } else {
+    // 사용자가 로그인되어 있지 않은 경우에 대한 처리
+    print('No user is currently logged in.');
+  }
+}
 
 final String currentMonth = DateFormat('MMMM').format(DateTime.now());
 
@@ -59,13 +144,20 @@ class graph extends StatefulWidget {
 }
 
 class _graphState extends State<graph> {
+  List<Map<String, dynamic>> expensesList = [];
+  List<Map<String, dynamic>> incomesList = [];
+  Future<List<Map<String, dynamic>>>? expensesFuture;
+  late Future<List<Map<String, dynamic>>> incomesFuture; //선주언니 페이지에서 가꼬온 코드(데이터베이스 가져오기)
+
+
   @override
   Widget build(BuildContext context) {
-    List<PieModel> model = [
+    List<PieModel> model = [   //파이차트 count 설정 여기서
       PieModel(count: 30, color: Color.fromARGB(255, 44, 183, 92).withOpacity(1)),
       PieModel(count: 10, color: Color.fromARGB(255, 253, 225, 14).withOpacity(1)),
       PieModel(count: 30, color: Color.fromARGB(255, 255, 199, 44).withOpacity(1)),
-      PieModel(count: 30, color: const Color.fromARGB(255, 214, 214, 214).withOpacity(1)),
+      PieModel(count: 10, color: Color.fromARGB(255, 140, 145, 217).withOpacity(1)),
+      PieModel(count: 20, color: const Color.fromARGB(255, 214, 214, 214).withOpacity(1)),
     ];
     
 
@@ -101,7 +193,7 @@ class _graphState extends State<graph> {
           children: [
             Positioned(
               left: 400.0, //여백 조절
-              top: 20.0, // 여백 및 이동 조절
+              top: 5.0, // 여백 및 이동 조절. 숫자 작아질수록 위로 가는듯
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬
@@ -109,7 +201,8 @@ class _graphState extends State<graph> {
             RowItem(color: Color.fromARGB(255, 44, 183, 92).withOpacity(1), label: '식비'),
             RowItem(color: Color.fromARGB(255, 255, 199, 44).withOpacity(1), label: '교통'),
             RowItem(color: Color.fromARGB(255, 253, 225, 14).withOpacity(1), label: '쇼핑'),
-            RowItem(color: const Color.fromARGB(255, 214, 214, 214).withOpacity(1), label: '기타'), //비율 가장 큰 지출항목 3개 반영할 계획
+            RowItem(color: Color.fromARGB(255, 140, 145, 217).withOpacity(1), label: '또뭐있노'),
+            RowItem(color: Color.fromARGB(255, 214, 214, 214).withOpacity(1), label: '기타'), //카테고리 최대 5개한다네용
             //지출항목에 대한 RowItem 추가
           ],
         ),
@@ -355,7 +448,7 @@ class _PieChart extends CustomPainter {
 
 
 
-void main() {
+void main()  {
   runApp(MaterialApp(
     home: graph(),
   ));
