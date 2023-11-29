1c15ad2e7521357af4f8dfe46bdc2fe03ee82cc3
sinsohi_04 <sohi8451@naver.com>
2023-11-30 00:28:29 +0900

    change_primarySwatch_color

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.grey),
+      theme: ThemeData(primarySwatch: Colors.green),
       home: WidgetTree(),
     );
   }
